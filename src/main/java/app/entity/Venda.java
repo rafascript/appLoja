2741a2757c9a5b2ae65822d67cbf70fd49504a97
BrunoV7 <backupnobre62@gmail.com>
2024-03-11 19:59:40 -0300

    Novas relações para as entities

--- src/main/java/app/entity/Venda.java
+++ src/main/java/app/entity/Venda.java
@@ -1,9 +1,16 @@
 package app.entity;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,9 +26,22 @@ public class Venda {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
-	private String endereco;
-	private double valor;
+	long id;
+	
+	String endereco;
+	double valor;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("vendas")
+	Cliente cliente;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("vendas")
+	Funcionario funcionario;
+	
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(name = "Venda_Produto")
+	private List<Produto> produtos;
 	
 	public long getId() {
 		return id;
