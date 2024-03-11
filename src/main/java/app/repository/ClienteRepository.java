6ee2241496ac5bd36c2b224eab5d5b1e8f3f8418
rafascript <rafa.eel1@live.com>
2024-03-11 19:46:24 -0300

    Repository added

--- src/main/java/app/repository/ClienteRepository.java
+++ src/main/java/app/repository/ClienteRepository.java
@@ -1,9 +1,19 @@
 package app.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import app.entity.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+	public List<Cliente> findByNome (String nome);
+	
+	public List<Cliente> findByCpf (String cpf);
+	
+	public List<Cliente> findByIdade (int idade);
+	
+	public List<Cliente> findByTelefone (String telefone);
 
 }
--- src/main/java/app/repository/VendaRepository.java
+++ src/main/java/app/repository/VendaRepository.java
@@ -1,9 +1,15 @@
 package app.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import app.entity.Venda;
 
 public interface VendaRepository extends JpaRepository<Venda, Long> {
 
+	public List<Venda> findByEnderecoDaEntrega (String enderecoDaEntrega);
+	
+	public List<Venda> findByValorTotal (double valor);
+	
 }
